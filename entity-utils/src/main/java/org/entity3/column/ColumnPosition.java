58b2eae5ad872d03fb84fdbed3a4693b716e1d3d
bochkov <bochkov2006@yandex.ru>
2017-02-23 11:56:35 +1200

    добавил сортировку колонок для eclipselink

---
+++ entity-utils/src/main/java/org/entity3/column/ColumnPosition.java
@@ -0,0 +1,15 @@
+package org.entity3.column;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by home on 23.02.17.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ColumnPosition {
+    int position();
+}
