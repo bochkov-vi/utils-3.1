e2f440f75c8eb99f458caf417c2dd9287765ae62
bochkov <bochkov@svpubo.ru>
2017-03-03 12:24:14 +1200

    Под situation подогнал маленько

---
+++ entity-utils/src/main/java/org/entity3/IdableEntity.java
@@ -0,0 +1,25 @@
+package org.entity3;
+
+import com.google.common.base.Objects;
+
+import java.io.Serializable;
+
+/**
+ * Created by bochkov on 03.03.17.
+ */
+public abstract class IdableEntity<ID extends Serializable> implements IIdable<ID> {
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IdableEntity<?> that = (IdableEntity<?>) o;
+        return Objects.equal(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
