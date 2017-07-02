706cb0537c53b84ce38161e458957bee75c3fd82
bochkov <bochkov2006@yandex.ru>
2017-07-02 23:44:22 +1200

    Merge branch 'master' of https://github.com/bochkov-vi/larisa
    Conflicts:
    	pom.xml

--- jsf-utils/src/main/java/jsf/util3/JsfEditService.java
+++ jsf-utils/src/main/java/jsf/util3/JsfEditService.java
@@ -1,43 +1,83 @@
 package jsf.util3;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Strings;
 import org.entity3.IIdable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.NestedRuntimeException;
 import org.springframework.data.domain.Persistable;
 
 import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.text.MessageFormat;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static jsf.util3.JsfUtil.addErrorMessage;
+import static jsf.util3.JsfUtil.addInfoMessage;
+
 /**
- * Created by bochkov on 30.06.17.
+ * Created by bochkov on 13.06.17.
  */
-public abstract class JsfEditService<T extends Persistable<ID> & IIdable<ID>, ID extends Serializable> extends ManagedConverter<T, ID> {
-    public static final String ERROR_ON_SAVE = "errorOnSave";
-    public static final String ERROR_ON_DELETE = "errorOnDelete";
-    public static final String ERROR_ON_EMPTY = "errorOnEmptySelected";
-    public static final String INFO_ON_SAVE = "infoOnObjectSave";
-    public static final String INFO_ON_OBJECT_EXISTS = "infoOnObjectExists";
-    protected String idParameterName = "id";
+@Configurable
+public abstract class JsfEditService<T extends Persistable<ID> & IIdable<ID>, ID extends Serializable> extends ManagedConverter<T, ID> implements JsfEditor<T, ID> {
     @Autowired
     @Qualifier("jsf-util-messages")
-    protected Properties msg;
+    transient protected Properties msg;
 
-    public JsfEditService() {
-        super();
-    }
+    protected String idParameterName = "id";
 
     public JsfEditService(Class<T> entityClass, Class<ID> idClass) {
         super(entityClass, idClass);
+    }
+
+    public JsfEditService() {
+        super();
     }
 
     public JsfEditService(Class<T> entityClass) {
         super(entityClass);
     }
 
+    @Override
+    public void delete(T selected) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getFlash().setKeepMessages(true);
+        if (selected != null) {
+            try {
+                getRepository().delete(selected);
+                selected = null;
+            } catch (Exception e) {
+                JsfUtil.addErrorMessage(msg.getProperty(ERROR_ON_DELETE), ((NestedRuntimeException) e).getRootCause());
+            }
+        } else {
+            addErrorMessage(msg.getProperty(ERROR_ON_EMPTY));
+        }
+    }
+
+    @Override
+    public T save(T selected) {
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getFlash().setKeepMessages(true);
+        try {
+            selected = getRepository().saveAndFlush(selected);
+            addInfoMessage(MessageFormat.format("{0}{1}", msg.getProperty(INFO_ON_SAVE), selected.getId()));
+        } catch (NestedRuntimeException e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, ERROR_ON_SAVE, e);
+            addErrorMessage(msg.getProperty(ERROR_ON_SAVE), e.getRootCause());
+        } catch (Exception e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, ERROR_ON_SAVE, e);
+            addErrorMessage(msg.getProperty(ERROR_ON_SAVE), MoreObjects.firstNonNull(e.getCause(), e));
+        }
+        return selected;
+    }
+
+
+    @Override
     public T entityFromRequest(String parameterName) {
         T result = null;
         FacesContext fc = FacesContext.getCurrentInstance();
@@ -50,10 +90,13 @@ public abstract class JsfEditService<T extends Persistable<ID> & IIdable<ID>, ID
         return result;
     }
 
+    @Override
     public T entityFromRequest() {
         return entityFromRequest(idParameterName);
     }
 
+
+    @Override
     public T createNewInstance() {
         try {
             return entityClass.newInstance();
@@ -65,6 +108,7 @@ public abstract class JsfEditService<T extends Persistable<ID> & IIdable<ID>, ID
         return null;
     }
 
+    @Override
     public String getIdParameterName() {
         return idParameterName;
     }
---
+++ jsf-utils/src/main/java/jsf/util3/JsfEditor.java
@@ -0,0 +1,29 @@
+package jsf.util3;
+
+import org.entity3.IIdable;
+import org.springframework.data.domain.Persistable;
+
+import java.io.Serializable;
+
+/**
+ * Created by bochkov on 02.07.17.
+ */
+public interface JsfEditor<T extends Persistable<ID> & IIdable<ID>, ID extends Serializable> {
+    String ERROR_ON_SAVE = "errorOnSave";
+    String ERROR_ON_DELETE = "errorOnDelete";
+    String ERROR_ON_EMPTY = "errorOnEmptySelected";
+    String INFO_ON_SAVE = "infoOnObjectSave";
+    String INFO_ON_OBJECT_EXISTS = "infoOnObjectExists";
+
+    void delete(T selected);
+
+    T save(T selected);
+
+    T entityFromRequest(String parameterName);
+
+    T entityFromRequest();
+
+    T createNewInstance();
+
+    String getIdParameterName();
+}
